6af6ca34eb07c37d414a0255ee76f75a4cccbe55
cpaca <37684627+cpaca@users.noreply.github.com>
2022-03-18 22:38:08 -0700

    Word collection

--- src/com/tsumiki/Main.java
+++ src/com/tsumiki/Main.java
@@ -1,8 +1,49 @@
 package com.tsumiki;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        ArrayList<String> wordList = new ArrayList<>();
+        BufferedReader reader = null;
+        try{
+            reader = new BufferedReader(new FileReader("words.txt"));
+            String line = reader.readLine();
+            while(line != null){
+                wordList.add(line);
+
+                line = reader.readLine();
+            }
+        } catch (IOException ioe){ // also file not found exception apparantly
+            ioe.printStackTrace();
+        } finally {
+            try{
+                if(reader != null){
+                    reader.close();
+                }
+            } catch (IOException ioe){
+                ioe.printStackTrace();
+            }
+        }
+
+        char[][] words = new char[wordList.size()][5];
+        AtomicInteger i = new AtomicInteger();
+
+        wordList.forEach((String s) ->{
+            char[] word = s.toCharArray();
+            words[i.get()] = word;
+
+            i.getAndIncrement();
+        });
+
+        for (char[] word : words) {
+            for (char c : word) {
+                System.out.print(c);
+            }
+            System.out.println(" - ");
+        }
     }
 }
