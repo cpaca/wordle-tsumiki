e78b212033f1df20aeaff04eab51d65569dcdecb
cpaca <37684627+cpaca@users.noreply.github.com>
2022-03-21 00:11:23 -0700

    Better ApplyGuess tests
    Also fixes #1
    
    In the future, rely more on the PyCharm program you made. In other words, treat it like the holy grail of tests, wherever possible.

--- src/com/tsumiki/Tests.java
+++ src/com/tsumiki/Tests.java
@@ -1,56 +1,20 @@
 package com.tsumiki;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Tests {
-
-    public static List<Integer> failedTests = new ArrayList<>();
-
     public static void TestOne(){
         WordleGuesser wordle = new WordleGuesser(Main.GetWords());
         char[] word;
         long quality;
-        System.out.println("NEXT TEST");
-        System.out.println("NEXT TEST");
-        System.out.println("NEXT TEST");
-        System.out.println("NEXT TEST");
-        System.out.println("NEXT TEST");
 
-        word = wordle.FindBestGuess();
-        quality = wordle.QualifyGuess(word);
-        System.out.print("Best guess: ");
-        System.out.println(word);
-        System.out.println("Quality: " + quality + "\n");
+        assert wordle.getAnswers().size() == 12972;
 
         wordle.ApplyGuess("track".toCharArray(), new byte[]{0,0,1,0,0});
-        word = wordle.FindBestGuess();
-        quality = wordle.QualifyGuess(word);
-        System.out.print("Best guess: ");
-        System.out.println(word);
-        System.out.println("Quality: " + quality + "\n");
+        assert wordle.getAnswers().size() == 1647;
 
         wordle.ApplyGuess("among".toCharArray(), new byte[]{2,0,1,0,0});
-        word = wordle.FindBestGuess();
-        quality = wordle.QualifyGuess(word);
-        System.out.print("Best guess: ");
-        System.out.println(word);
-        System.out.println("Quality: " + quality + "\n");
+        assert wordle.getAnswers().size() == 16;
 
         wordle.ApplyGuess("lapse".toCharArray(), new byte[]{1,1,0,0,0});
-        word = wordle.FindBestGuess();
-        quality = wordle.QualifyGuess(word);
-        System.out.print("Best guess: ");
-        System.out.println(word);
-        System.out.println("Quality: " + quality + "\n");
-    }
-
-    public static void PrintFailedTests(){
-        for(Integer i : failedTests){
-            System.out.println("Failed test: " + i);
-        }
-        if(failedTests.size() == 0){
-            System.out.println("No failed tests.");
-        }
+        assert wordle.getAnswers().size() == 6;
     }
 }
