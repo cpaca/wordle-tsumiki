8429cfe3df17e18742b9e69afffe1b12952ac3bf
cpaca <37684627+cpaca@users.noreply.github.com>
2022-03-20 17:52:14 -0700

    Prototype Apply Guess probably unnecessary now

--- src/com/tsumiki/Main.java
+++ src/com/tsumiki/Main.java
@@ -8,70 +8,7 @@ import java.util.function.BiPredicate;
 public class Main {
 
     public static void main(String[] args){
-        boolean PassTests = ConfirmProtoGuess();
-        if(!PassTests){
-            System.out.println("Prototype function is not identical.");
-        }
-        else{
-            System.out.println("Prototype function passes");
-        }
 
-        TestApplyGuessSpeed();
-        TestProtoGuessSpeed();
-    }
-
-    private static final int iters = 1000;
-    private static void TestApplyGuessSpeed(){
-        long time = 0;
-        for(int i = 0; i < iters; i++){
-            WordleGuesser wordle = new WordleGuesser(GetWords());
-            time -= System.nanoTime();
-            wordle.ApplyGuess("adieu".toCharArray(), new byte[]{1,0,0,1,1});
-            time += System.nanoTime();
-        }
-        time /= iters;
-        System.out.println("Method ApplyGuess");
-        System.out.printf("Average time: %,d\n", time);
-    }
-
-    private static void TestProtoGuessSpeed(){
-        long time = 0;
-        for(int i = 0; i < iters; i++){
-            WordleGuesser wordle = new WordleGuesser(GetWords());
-            time -= System.nanoTime();
-            wordle.ProtoApplyGuess("adieu".toCharArray(), new byte[]{1,0,0,1,1});
-            time += System.nanoTime();
-        }
-        time /= iters;
-        System.out.println("Method ProtoApplyGuess");
-        System.out.printf("Average time: %,d\n", time);
-    }
-
-    private static boolean ConfirmProtoGuess(){
-        BiPredicate<String, byte[]> cons = (String s, byte[] b) -> {
-            WordleGuesser wordle1 = new WordleGuesser(GetWords());
-            WordleGuesser wordle2 = new WordleGuesser(GetWords());
-
-            wordle1.ApplyGuess(s.toCharArray(), b);
-            wordle2.ApplyGuess(s.toCharArray(), b);
-
-            boolean out = wordle1.getAnswers().size() == wordle2.getAnswers().size();
-            if(!out){
-                System.out.println("Test failed: " + s);
-            }
-            return out;
-        };
-
-        boolean out = cons.test("adieu", new byte[] {1,0,0,1,1});
-        out = out && cons.test("among", new byte[] {0,0,0,1,0});
-        out = out && cons.test("nicer", new byte[] {1,0,0,2,1});
-        out = out && cons.test("rebel", new byte[] {2,2,0,2,0});
-        out = out && cons.test("renew", new byte[] {2,2,2,2,2});
-
-        // Using non-words now just to test edge cases.
-        out = out && cons.test("anney", new byte[] {1,1,2,0,2});
-
-        return out;
     }
 
     private static char[][] GetWords(){
--- src/com/tsumiki/WordleGuesser.java
+++ src/com/tsumiki/WordleGuesser.java
@@ -153,10 +153,6 @@ public class WordleGuesser {
         _answers = filteredWords;
     }
 
-    public void ProtoApplyGuess(final char[] inGuess, final byte[] inState){
-        ApplyGuess(inGuess, inState);
-    }
-
     public List<char[]> getAnswers() {
         return _answers;
     }
