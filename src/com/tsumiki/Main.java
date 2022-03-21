6c50ab26fd9a92bcf2decf32b62f69f4a8a14e4f
cpaca <37684627+cpaca@users.noreply.github.com>
2022-03-20 18:32:37 -0700

    QualifyGuess timer - 0.3 ms/call

--- src/com/tsumiki/Main.java
+++ src/com/tsumiki/Main.java
@@ -2,12 +2,41 @@ package com.tsumiki;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 
 public class Main {
 
     public static void main(String[] args){
+        TimeQualifier();
+    }
+
+    private static final int iters = 10000;
+    private static void TimeQualifier(){
+        long time = 0;
+        WordleGuesser wordle = new WordleGuesser(GetWords());
+        char[][] words = GetWords();
+        Random r = new Random(System.nanoTime());
+        for(int i = 0; i < iters; i++){
+            int wordIndex = r.nextInt(words.length);
+            char[] word = words[wordIndex];
+            time -= System.nanoTime();
+            wordle.QualifyGuess(word);
+            time += System.nanoTime();
+        }
+
+        System.out.printf("Maximum value LONG: %,d\n", Long.MAX_VALUE);
+        System.out.printf("Current time:       %,d\n", System.nanoTime());
+        System.out.printf("Total time taken:   %,d\n", time);
+        time /= iters;
+        System.out.printf("Average time taken: %,d\n", time);
+    }
+
+    private static void TestProtoQualifier(){
+
+        // Code from when I first made the qualifier
+        // Modify it a tiny bit if you want to test the proto-qualifier
         WordleGuesser wordle = new WordleGuesser(GetWords());
         Consumer<String> qualifier = (String s) ->{
             System.out.println("Quality of " + s + ": " + wordle.QualifyGuess(s.toCharArray()));
