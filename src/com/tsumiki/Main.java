1bb62b0acae32924714334c96f9e2dc2ccca1ba6
cpaca <37684627+cpaca@users.noreply.github.com>
2022-03-21 11:20:53 -0700

    Add Quordle tests
    Really, this just moves the existing Quordle tests in Main into the Tests file.
    
    So I can run its assert whenever I like.

--- src/com/tsumiki/Main.java
+++ src/com/tsumiki/Main.java
@@ -8,74 +8,7 @@ public class Main {
 
     public static void main(String[] args){
         Tests.TestOne();
-
-        MultiWordleGuesser quordle = new MultiWordleGuesser(4, GetWords());
-
-        quordle.ApplyGuess("lares".toCharArray(), new byte[][]{
-                new byte[] {1,0,0,0,0},
-                new byte[] {0,0,1,1,0},
-                new byte[] {0,1,0,0,2},
-                new byte[] {0,0,0,1,1},
-        });
-
-        quordle.ApplyGuess("colly".toCharArray(), new byte[][]{
-                new byte[] {0,0,1,0,0},
-                new byte[] {2,1,0,0,0},
-                new byte[] {0,0,0,0,0},
-                new byte[] {0,1,0,0,0},
-        });
-
-        quordle.ApplyGuess("pling".toCharArray(), new byte[][]{
-                new byte[] {0,2,0,0,0},
-                new byte[] {0,0,0,2,0},
-                new byte[] {0,0,0,0,0},
-                new byte[] {0,0,0,2,0},
-        });
-
-        quordle.ApplyGuess("crone".toCharArray(), new byte[][]{
-                new byte[] {0,0,0,0,0},
-                new byte[] {2,2,2,2,2},
-                new byte[] {0,0,0,0,0},
-                new byte[] {0,0,2,2,2},
-        });
-
-        quordle.ApplyGuess("bluff".toCharArray(), new byte[][]{
-                new byte[] {0,2,2,2,2},
-                // new byte[] {2,2,2,2,2},
-                new byte[] {0,0,0,0,0},
-                new byte[] {0,0,0,0,0},
-        });
-
-        quordle.ApplyGuess("fluff".toCharArray(), new byte[][]{
-                new byte[] {2,2,2,2,2},
-                // new byte[] {2,2,2,2,2},
-                new byte[] {0,0,0,0,0},
-                new byte[] {0,0,0,0,0},
-        });
-
-        quordle.ApplyGuess("shone".toCharArray(), new byte[][]{
-                // new byte[] {2,2,2,2,2},
-                // new byte[] {2,2,2,2,2},
-                new byte[] {1,0,0,0,0},
-                new byte[] {2,0,2,2,2},
-        });
-
-        quordle.ApplyGuess("stone".toCharArray(), new byte[][]{
-                // new byte[] {2,2,2,2,2},
-                // new byte[] {2,2,2,2,2},
-                new byte[] {1,0,0,0,0},
-                new byte[] {2,2,2,2,2},
-        });
-
-        quordle.ApplyGuess("amass".toCharArray(), new byte[][]{
-                // new byte[] {2,2,2,2,2},
-                // new byte[] {2,2,2,2,2},
-                new byte[] {2,2,2,2,2},
-                // new byte[] {2,0,2,2,2},
-        });
-
-        char[] word = quordle.FindBestGuess();
-        System.out.println(word);
+        Tests.TestTwo();
     }
 
     public static char[][] GetWords(){
@@ -111,17 +44,6 @@ public class Main {
             i.getAndIncrement();
         });
         return words;
-    }
-
-    public static boolean EqualCharArrays(char[] a, char[] b){
-        if(a.length != b.length){
-            return false;
-        }
-        for(int i = 0; i < a.length; i++){
-            if(a[i] != b[i])
-                return false;
-        }
-        return true;
     }
 
     public static void PrintIntArray(byte[] arr){
--- src/com/tsumiki/Tests.java
+++ src/com/tsumiki/Tests.java
@@ -15,4 +15,99 @@ public class Tests {
         wordle.ApplyGuess("lapse".toCharArray(), new byte[]{1,1,0,0,0});
         assert wordle.getAnswers().size() == 6;
     }
+
+    public static void TestTwo(){
+        MultiWordleGuesser quordle = new MultiWordleGuesser(4, Main.GetWords());
+
+        assert EqualCharArrays(quordle.FindBestGuess(), "lares");
+        quordle.ApplyGuess("lares".toCharArray(), new byte[][]{
+                new byte[] {1,0,0,0,0},
+                new byte[] {0,0,1,1,0},
+                new byte[] {0,1,0,0,2},
+                new byte[] {0,0,0,1,1},
+        });
+
+        assert EqualCharArrays(quordle.FindBestGuess(), "colly");
+        quordle.ApplyGuess("colly".toCharArray(), new byte[][]{
+                new byte[] {0,0,1,0,0},
+                new byte[] {2,1,0,0,0},
+                new byte[] {0,0,0,0,0},
+                new byte[] {0,1,0,0,0},
+        });
+
+        assert EqualCharArrays(quordle.FindBestGuess(), "pling");
+        quordle.ApplyGuess("pling".toCharArray(), new byte[][]{
+                new byte[] {0,2,0,0,0},
+                new byte[] {0,0,0,2,0},
+                new byte[] {0,0,0,0,0},
+                new byte[] {0,0,0,2,0},
+        });
+
+        assert EqualCharArrays(quordle.FindBestGuess(), "crone");
+        quordle.ApplyGuess("crone".toCharArray(), new byte[][]{
+                new byte[] {0,0,0,0,0},
+                new byte[] {2,2,2,2,2},
+                new byte[] {0,0,0,0,0},
+                new byte[] {0,0,2,2,2},
+        });
+
+        assert EqualCharArrays(quordle.FindBestGuess(), "bluff");
+        quordle.ApplyGuess("bluff".toCharArray(), new byte[][]{
+                new byte[] {0,2,2,2,2},
+                // new byte[] {2,2,2,2,2},
+                new byte[] {0,0,0,0,0},
+                new byte[] {0,0,0,0,0},
+        });
+
+        assert EqualCharArrays(quordle.FindBestGuess(), "fluff");
+        quordle.ApplyGuess("fluff".toCharArray(), new byte[][]{
+                new byte[] {2,2,2,2,2},
+                // new byte[] {2,2,2,2,2},
+                new byte[] {0,0,0,0,0},
+                new byte[] {0,0,0,0,0},
+        });
+
+        assert EqualCharArrays(quordle.FindBestGuess(), "shone");
+        quordle.ApplyGuess("shone".toCharArray(), new byte[][]{
+                // new byte[] {2,2,2,2,2},
+                // new byte[] {2,2,2,2,2},
+                new byte[] {1,0,0,0,0},
+                new byte[] {2,0,2,2,2},
+        });
+
+        assert EqualCharArrays(quordle.FindBestGuess(), "stone");
+        quordle.ApplyGuess("stone".toCharArray(), new byte[][]{
+                // new byte[] {2,2,2,2,2},
+                // new byte[] {2,2,2,2,2},
+                new byte[] {1,0,0,0,0},
+                new byte[] {2,2,2,2,2},
+        });
+
+        assert EqualCharArrays(quordle.FindBestGuess(), "amass");
+        quordle.ApplyGuess("amass".toCharArray(), new byte[][]{
+                // new byte[] {2,2,2,2,2},
+                // new byte[] {2,2,2,2,2},
+                new byte[] {2,2,2,2,2},
+                // new byte[] {2,0,2,2,2},
+        });
+    }
+
+    public static boolean EqualCharArrays(char[] a, String b){
+        return EqualCharArrays(a, b.toCharArray());
+    }
+
+    public static boolean EqualCharArrays(String a, char[] b){
+        return EqualCharArrays(a.toCharArray(), b);
+    }
+
+    public static boolean EqualCharArrays(char[] a, char[] b){
+        if(a.length != b.length){
+            return false;
+        }
+        for(int i = 0; i < a.length; i++){
+            if(a[i] != b[i])
+                return false;
+        }
+        return true;
+    }
 }
