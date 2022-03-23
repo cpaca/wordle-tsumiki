80ad5cbdabe2e0777ce0e3f3de954934961da7c1
cpaca <37684627+cpaca@users.noreply.github.com>
2022-03-22 22:58:13 -0700

    Why is QualifyGuess faster now?

--- src/com/tsumiki/Main.java
+++ src/com/tsumiki/Main.java
@@ -1,13 +1,35 @@
 package com.tsumiki;
 
 import java.io.*;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
 
     public static void main(String[] args){
         Tests.RunTests();
+
+        TimeQualifyGuess();
+    }
+
+    public static final int iters = 10000;
+    public static void TimeQualifyGuess(){
+        long totalTime = 0;
+        WordleGuesser wordle = new WordleGuesser(GetWords());
+        DecimalFormat format = new DecimalFormat("0,000,000,000");
+        Random r = new Random(System.nanoTime());
+        char[][] words = GetWords();
+        for(int i = 0; i < iters; i++){
+            char[] word = words[r.nextInt(words.length)];
+            totalTime -= System.nanoTime();
+            wordle.QualifyGuess(word);
+            totalTime += System.nanoTime();
+        }
+        System.out.println("Long.MAX_VALUE: " + format.format(Long.MAX_VALUE));
+        System.out.println("Total time:     " + format.format(totalTime));
+        System.out.println("Average time:   " + format.format(totalTime/iters));
     }
 
     public static char[][] GetWords(){
