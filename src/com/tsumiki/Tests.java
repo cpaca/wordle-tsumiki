26e49cf59f124e3032cc780ae79eafa1a7005ba8
cpaca <37684627+cpaca@users.noreply.github.com>
2022-03-24 14:36:17 -0700

    Re-added Quality tests
    Quality tests are now backed by ApplyGuess, which is confirmed true by the PythonTests

--- src/com/tsumiki/Tests.java
+++ src/com/tsumiki/Tests.java
@@ -1,7 +1,9 @@
 package com.tsumiki;
 
+import java.lang.reflect.GenericArrayType;
 import java.lang.reflect.Method;
 import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 import static com.tsumiki.PythonTests.RunPythonTests;
 
@@ -11,6 +13,7 @@ public class Tests {
         RunPythonTests();
 
         TestOne();
+        TestTwo();
         TestThree();
     }
 
@@ -27,6 +30,48 @@ public class Tests {
 
         wordle.ApplyGuess("lapse".toCharArray(), new byte[]{1,1,0,0,0});
         assert wordle.getAnswers().size() == 6;
+    }
+
+    private static void TestTwo(){
+        Consumer<String> cons = (String s) -> {
+            WordleGuesser wordle = new WordleGuesser(Main.GetWords());
+            long theorQual = wordle.QualifyGuess(s.toCharArray());
+
+            int[] states = new int[3*3*3*3*3];
+            byte[] state = {0, 0, 0, 0, 0};
+            int stateBase3 = 0;
+            int index = 0;
+
+            while(index < state.length){
+                wordle = new WordleGuesser(Main.GetWords());
+                wordle.ApplyGuess(s.toCharArray(), state);
+                states[stateBase3] = wordle.getAnswers().size();
+
+                index = 0;
+                stateBase3++;
+                while(index < state.length && state[index] == 2){
+                    state[index] = 0;
+                    index++;
+                }
+                if(index < state.length){
+                    state[index]++;
+                }
+            }
+
+            long actualQual = 0;
+            for(int i = 0; i < states.length-1; i++){
+                actualQual += states[i] * states[i];
+            }
+            assert actualQual == theorQual;
+        };
+
+        cons.accept("adieu");
+        cons.accept("snort");
+        cons.accept("frays");
+        cons.accept("tests");
+        cons.accept("zzzzz");
+        cons.accept("lares");
+        cons.accept("meows");
     }
 
     private static void TestThree(){
